99a0f0e2c0e0ebe73e3a402ae90549c07a125a1f
lopezivan763 <84734876+lopezivan763@users.noreply.github.com>
2021-06-01 09:30:52 -0500

    Update Activity 1.py

--- Assignment 3/Activity 1.py
+++ Assignment 3/Activity 1.py
@@ -5,7 +5,7 @@ print("Enter the hourly pay rate")
 payRate = float(input())
 grossPay = hours * payRate
 print("The employee gross pay is $" + str(grossPay))
-weekly = hours * payRate * 7
+weekly = hours * payRate * 5
 montly = weekly * 4
 anual = weekly * 52
 print("The employee weekly pay is $" + str(weekly) + ". The employee montly pay is $" + str(montly) + ". The employee anual pay is $" + str(anual) + ".")
