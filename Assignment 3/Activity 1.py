31eebebf268e3295a11b789982845538160af252
lopezivan763 <84734876+lopezivan763@users.noreply.github.com>
2021-06-01 10:45:03 -0500

    Update Activity 1.py

--- Assignment 3/Activity 1.py
+++ Assignment 3/Activity 1.py
@@ -12,8 +12,8 @@ grossPay = hours * payRate
 
 print("The employee gross pay is $" + str(grossPay))
 
-montly = grossPay * 4
+monthly = grossPay * 4
 
-anual = grossPay * 52
+yearly = grossPay * 52
 
-print("The employee weekly pay is $" + str(grossPay) + ". The employee montly pay is $" + str(montly) + ". The employee anual pay is $" + str(anual) + ".")
+print("The employee weekly pay is $" + str(grossPay) + ". The employee montly pay is $" + str(monthly) + ". The employee anual pay is $" + str(yearly) + ".")
