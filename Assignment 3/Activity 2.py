de362633552872f54aca9134f243e8f8cebd0930
lopezivan763 <84734876+lopezivan763@users.noreply.github.com>
2021-05-31 21:37:20 -0500

    Add files via upload

---
+++ Assignment 3/Activity 2.py
@@ -0,0 +1,5 @@
+﻿# This program converts an input Fahrenheit temperature to Celsius.
+print("Enter the Fahrenheit temperature")
+fahrenheit = float(input())
+celsius = (fahrenheit - 32) * 5 / 9
+print(str(fahrenheit) + "º Fahrenheit is " + str(celsius) + "º Celsius")
