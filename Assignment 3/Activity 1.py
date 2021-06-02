bdd07132be29136fcc2457a1ac2a051edb7c6db4
Dave Braunschweig <dbraunsc@harpercollege.edu>
2021-06-02 11:52:56 -0500

    Assignment 3 corrections

--- Assignment 3/Activity 1.py
+++ Assignment 3/Activity 1.py
@@ -1,19 +1,17 @@
-# This program converts an input hourly pay rate to a weekly, montly and anual income
+# This program converts an input hourly pay rate to a weekly,
+# montly and anual income.
 
 print("Enter the number of hours")
-
 hours = float(input())
 
 print("Enter the hourly pay rate")
-
 payRate = float(input())
 
 grossPay = hours * payRate
-
-print("The employee gross pay is $" + str(grossPay))
-
 monthly = grossPay * 4
-
 yearly = grossPay * 52
 
-print("The employee weekly pay is $" + str(grossPay) + ". The employee monthly pay is $" + str(monthly) + ". The employee yearly pay is $" + str(yearly) + ".")
+print("The employee gross pay is $" + str(grossPay))
+print("The employee weekly pay is $" + str(grossPay) +
+    ". The employee monthly pay is $" + str(monthly) +
+    ". The employee yearly pay is $" + str(yearly) + ".")
