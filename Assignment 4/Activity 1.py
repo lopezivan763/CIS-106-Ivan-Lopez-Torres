4f0d56425ef3b36d1919f640289e6bc3f2037057
lopezivan763 <84734876+lopezivan763@users.noreply.github.com>
2021-06-02 11:46:42 -0500

    Add files via upload

---
+++ Assignment 4/Activity 1.py
@@ -0,0 +1,26 @@
+# This program calculates miles -
+# to yard, feet, and inches
+
+print("What is the distance in miles?")
+
+miles = float(input())
+
+print("The distance is " + str(miles) + " miles.")
+
+yards = miles * 1760
+
+feet = miles * 5280
+
+inches = miles * 63360
+
+print('')
+
+print("The distance from miles to yards is " + str(yards) + " yards.")
+
+print('')
+
+print("The distance from miles to feet is " + str(feet) + " feet.")
+
+print('')
+
+print("The distance from miles to inches is " + str(inches) + " inches.")
