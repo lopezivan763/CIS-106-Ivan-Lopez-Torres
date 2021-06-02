b80286db83c83c2ef9673639bcc799f69884c607
lopezivan763 <84734876+lopezivan763@users.noreply.github.com>
2021-06-02 09:55:31 -0500

    Update Activity 2.py

--- Assignment 3/Activity 2.py
+++ Assignment 3/Activity 2.py
@@ -1,19 +1,36 @@
 #     program that calculates the age of the user to years months, days, etc.
 
-  print("How old are you?")
+
+print("How old are you?")
+
 print('')
-  age = float(input())
-  print("The users age is " + str(age) + " year's old.")
+
+age = float(input())
+  
+print("The users age is " + str(age) + " year's old.")
+
 print('')
-  months = age * 12
-  days = age * 365
-  hours = age * 8760
-  seconds = age * 31536000
+
+months = age * 12
+
+days = age * 365
+
+hours = age * 8760
+
+seconds = age * 31536000
+
 print('')
-  print("The user age in months is " + str(months) + " months.")
+
+print("The user age in months is " + str(months) + " months.")
+
 print('')
-  print("The user age in days is " + str(days) + " days.")
+
+print("The user age in days is " + str(days) + " days.")
+  
 print('')
-  print("The user age in hours is " + str(hours) + " hours.")
+
+print("The user age in hours is " + str(hours) + " hours.")
+  
 print('')
-  print("The user age in seconds is " + str(seconds) + " seconds.")
+
+print("The user age in seconds is " + str(seconds) + " seconds.")
