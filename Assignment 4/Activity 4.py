72da27970cf80e97fce1da97ba76f5b7ef26d940
lopezivan763 <84734876+lopezivan763@users.noreply.github.com>
2021-06-03 12:19:10 -0500

    Add files via upload

---
+++ Assignment 4/Activity 4.py
@@ -0,0 +1,37 @@
+# This is a basic program that
+# turns dog years into human years
+
+
+print("what is the name of the dog?")
+
+name = input()
+
+print("How old is " + str(name) + "?")
+
+age = float(input())
+
+print(str(name) + " is " + str(age) + " years old.")
+
+print("What size is your dog?" " small medium, or large?")
+
+size = input()
+
+print(str(name) + " is a " + str(size) + " dog")
+print('')
+print("What type is your dog?")
+
+type = input()
+
+print(str(name) + " is a " + str(type) + ".")
+print('')
+print("1 dog year equals to 7 human year.")
+print('')
+human = age * 7
+
+print(str(name) + " is a " + str(size) + " dog.")
+print('')
+print(str(name) + " is a " + str(type) + ".")
+print('')
+print(str(name) + " is " + str(human) + " in human years.")
+print('')
+print(str(name) + " is " + str(age) + " years old in dog year.")
