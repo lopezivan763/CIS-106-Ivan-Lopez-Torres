d75894033dac15c8660b38e291ed36dac11b0254
lopezivan763 <84734876+lopezivan763@users.noreply.github.com>
2021-06-08 11:49:48 -0500

    Update Activity 7.py

--- Assignment 5/Activity 7.py
+++ Assignment 5/Activity 7.py
@@ -2,6 +2,7 @@
 # to dog years
 
 def calc_age(human):
+
     age = float(human) / 7
 
     return age
@@ -10,18 +11,21 @@ def display_result(name, human, age):
     print(name + " is " + str(age) + " years old in dog years.")
 
 def get_human():
+
     print("Enter the dog's age in human years")
     human = int(input())
 
     return human
 
 def get_name():
+
     print("Enter the dog's name")
     name = input()
 
     return name
 
 def main():
+
     name = get_name()
     human = get_human()
     age = calc_age(human)
