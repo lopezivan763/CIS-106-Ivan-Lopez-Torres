ce9175d363a8eee5754cae82212179d6a507da2f
lopezivan763 <84734876+lopezivan763@users.noreply.github.com>
2021-06-08 22:54:28 -0500

    Update Activity 1.py

--- Assignment 6/Activity 1.py
+++ Assignment 6/Activity 1.py
@@ -1,43 +1,53 @@
-def calcAnual(weekly):
+# This programs changes your gross pay to weekly,
+# montly, and anual pay
+
+
+def calc_anual(weekly):
     anual = weekly * 52
     
     return anual
 
-def calcMonthly(weekly):
+
+def calc_monthly(weekly):
     monthly = weekly * 4
     
     return monthly
 
-def calcPay(hours, rate):
+
+def calc_pay(hours, rate):
     pay = hours * rate
     
     return pay
 
-def calcWeekly(hours, rate):
+
+def calc_weekly(hours, rate):
     weekly = hours * rate * 7
     
     return weekly
 
-def displayResult(pay, weekly, monthly, anual):
+
+def display_result(pay, weekly, monthly, anual):
     print("The employee gross pay is $" + str(pay) + "." + " The employee weekly pays is $" + str(weekly) + "." + " The employee montly pay is $" + str(monthly) + "." + " The employee anual pay is $" + str(anual) + ".")
 
-def getHours():
+    
+def get_hours():
     print("Enter the number of hours")
     hours = float(input())
     
     return hours
 
-def getRate():
+
+def get_rate():
     print("Enter the hourly rate")
     rate = float(input())
     
     return rate
 
-# Main
-hours = getHours()
-rate = getRate()
-pay = calcPay(hours, rate)
-weekly = calcWeekly(hours, rate)
-monthly = calcMonthly(weekly)
-anual = calcAnual(weekly)
-displayResult(pay, weekly, monthly, anual)
+
+hours = get_hours()
+rate = get_rate()
+pay = calc_pay(hours, rate)
+weekly = calc_weekly(hours, rate)
+monthly = calc_monthly(weekly)
+anual = calc_anual(weekly)
+display_result(pay, weekly, monthly, anual)
