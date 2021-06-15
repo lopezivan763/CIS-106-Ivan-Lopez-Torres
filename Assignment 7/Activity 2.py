460aad20f67a129778b4542e342dac9b9671967c
lopezivan763 <84734876+lopezivan763@users.noreply.github.com>
2021-06-15 13:25:31 -0500

    Update Activity 2.py

--- Assignment 7/Activity 2.py
+++ Assignment 7/Activity 2.py
@@ -2,69 +2,69 @@
 # age into months, days, hours, or seconds.
 
 
-def calcDays(age):
+def calc_days(age):
     days = age * 365
     
     return days
 
-def calcHours(age):
+def calc_hours(age):
     hours = age * 8760
     
     return hours
 
-def calcMonths(age):
+def calc_months(age):
     months = age * 12
     
     return months
 
-def calcSeconds(age):
+def calc_seconds(age):
     seconds = age * 31536000
     
     return seconds
 
-def displayDays(days):
+def display_days(days):
     print("Your age in days is " + str(days) + " days.")
 
-def displayHours(hours):
+def display_hours(hours):
     print("Your age in hours is " + str(hours) + " hours.")
 
-def displayMonths(months):
+def display_months(months):
     print("Your age in months is " + str(months) + " months.")
 
-def displaySeconds(seconds):
+def display_seconds(seconds):
     print("Your age in seconds is " + str(seconds) + " seconds.")
 
-def getAge():
+def get_age():
     print("Enter your age")
     age = float(input())
     
     return age
 
-def getChoice():
-    print("Would you like to know yor age in months, days, hours, or seconds?")
+def get_choice():
+    print("Would you like to know your age in months, days, hours, or seconds?")
     choice = input()
     
     return choice
 
 def main():
-    age = getAge()
-    choice = getChoice()
-    months = calcMonths(age)
-    days = calcDays(age)
-    hours = calcHours(age)
-    seconds = calcSeconds(age)
-if choice == "months":
-    displayMonths(months)
-else:
-    if choice == "days":
-        displayDays(days)
+    age = get_age()
+    choice = get_choice()
+    months = calc_months(age)
+    days = calc_days(age)
+    hours = calc_hours(age)
+    seconds = calc_seconds(age)
+    if choice == "months":
+       display_months(months)
     else:
-        if choice == "hours":
-            displayHours(hours)
-        else:
-            if choice == "seconds":
-                displaySeconds(seconds)
-            else:
-                print("Please select if you want your age to be display in months, days, hours, or seconds")
+    if choice == "days":
+       display_days(days)
+    else:
+    if choice == "hours":
+       display_hours(hours)
+    else:
+    if choice == "seconds":
+       display_seconds(seconds)
+    else:
+       print("Please select if you want your age to be display in months, days, hours, or seconds")
                 
 main()
